fd64b4b66d258981a6e8fcd27a20c4e9f6606751
Jerome Bell <jeromebell@gmail.com>
2024-03-03 20:09:23 -0600

    Web share (#4)

--- src/MWRCheatSheet/Program.cs
+++ src/MWRCheatSheet/Program.cs
@@ -1,3 +1,4 @@
+using Append.Blazor.WebShare;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -11,6 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
+builder.Services.AddWebShare();
 
 builder.Services.AddBlazoredLocalStorage();
 
