991eb292fa6e3a9a03ad5bd8af8c4847631eaae2
Jerome Bell <jeromebell@gmail.com>
2024-05-19 11:05:36 -0500

    Trust Fund My Baby (#48)

---
+++ src/MegaSchool1.Model/ProductionDuplicationLevel.cs
@@ -0,0 +1,14 @@
+﻿namespace MegaSchool1.Model;
+
+public record ProductionDuplicationLevel(int PersonalMemberEnrollments, ProductionDuplicationLevel? PreviousProgress)
+{
+    public int TeamMembershipEnrollments { get; init; }
+        =
+        PersonalMemberEnrollments
+        +
+        // Downline membership enrollments (the entire team got 1 enrollment)
+        (PreviousProgress?.TeamMembershipEnrollments ?? 0)
+        +
+        // Previous team membersip enrollments
+        (PreviousProgress?.TeamMembershipEnrollments ?? 0);
+}
