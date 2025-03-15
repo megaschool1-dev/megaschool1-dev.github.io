4ff5d493dae4a7f0100ad7302c48cce78f094d69
Jerome Bell <jeromebell@gmail.com>
2025-03-15 14:49:43 -0500

    WebAssembly prerender mode

--- src/MegaSchool1/Program.cs
+++ src/MegaSchool1/Program.cs
@@ -10,9 +10,16 @@ using Microsoft.AspNetCore.Components;
 using Serilog;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Determine whether root components are already registered via prerednered HTML contents.
+// See https://github.com/jsakamoto/BlazorWasmPreRendering.Build
+if (!builder.RootComponents.Any())
+{
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
+}
+
+// Do not change this line, it's for prerendering. See https://github.com/jsakamoto/BlazorWasmPreRendering.Build
 ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress, builder.Configuration);
 
 Log.Logger = new LoggerConfiguration()
@@ -23,6 +30,8 @@ Log.Logger = new LoggerConfiguration()
 
 await builder.Build().RunAsync();
 
+// This method signature follows a convention to enable prerendering.
+// See https://github.com/jsakamoto/BlazorWasmPreRendering.Build
 static void ConfigureServices(IServiceCollection services, string baseAddress, IConfiguration configuration)
 {
     var http = new HttpClient { BaseAddress = new Uri(baseAddress) };
